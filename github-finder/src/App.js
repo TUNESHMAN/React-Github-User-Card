6e7dbec84b08af91bdffe09016c6b0a0f9ebe908
Babatunde <babatundea15@gmail.com>
2019-12-03 16:34:32 +0100

    I installed axios

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -2,8 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import Users from "./Components/Users";
+import axios from 'axios';
 
 class App extends Component {
+  componentDidMount(){
+    axios.get('https://api.github.com/users/')
+    .then((response)=>{
+      console.log(response.data);
+      
+
+    })
+    .catch((error)=>{
+console.log(error);
+
+    })
+  }
   render() {
     return (
       <nav className="navbar bg-primary">

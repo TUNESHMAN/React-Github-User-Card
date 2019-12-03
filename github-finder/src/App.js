b8568466f58fe3a8be2e06b450c8de08b5119d3d
Babatunde <babatundea15@gmail.com>
2019-12-03 15:59:53 +0100

    I created the user item and users component

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react'
-import './App.css';
-import NavBar from './Components/NavBar'
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Users from "./Components/Users";
 
 class App extends Component {
   render() {
     return (
       <nav className="navbar bg-primary">
-        <NavBar title="Github Finder" icon="fab fa-github"/>
-        {/* <h1>Hello from React</h1> */}
-
-        
+        <NavBar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Users />
+        </div>
       </nav>
-    )
+    );
   }
 }
 
-export default App
+export default App;

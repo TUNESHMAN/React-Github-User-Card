fb82578c4e8d6a49a554ad4c16735617de8c5043
Babatunde <babatundea15@gmail.com>
2019-12-03 14:15:48 +0100

    I created the Nav Bar component and styled it

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import './App.css';
+import NavBar from './Components/NavBar'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello from React</h1>
+      <nav className="navbar bg-primary">
+        <NavBar title="Github Finder" icon="fab fa-github"/>
+        {/* <h1>Hello from React</h1> */}
+
         
-      </div>
+      </nav>
     )
   }
 }

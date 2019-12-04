941e79e4dde599ead5ab8b52e431db70669bd436
Babatunde <babatundea15@gmail.com>
2019-12-04 01:01:24 +0100

    I fetched data from the github API and rendered on the screen

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -2,27 +2,38 @@ import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import Users from "./Components/Users";
-import axios from 'axios';
+import axios from "axios";
 
 class App extends Component {
-  componentDidMount(){
-    axios.get('https://api.github.com/users/')
-    .then((response)=>{
-      console.log(response.data);
-      
-
-    })
-    .catch((error)=>{
-console.log(error);
-
-    })
+  state = {
+    users: [],
+    loading: false
+  };
+  componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    const res = axios
+      .get(
+        "https://api.github.com/users?8999cb036cee4535f9e864266be4be92762e9098"
+      )
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          users: response.data,
+          loading: false
+        });
+      })
+      .catch(error => {
+        // console.log(error);
+      });
   }
   render() {
     return (
       <nav className="navbar bg-primary">
         <NavBar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </nav>
     );
